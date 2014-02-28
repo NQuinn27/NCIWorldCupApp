1e06ab475d61a95e4c9a923b8336f0fb8c344c7f
Niall Quinn <niall.quinn.27@gmail.com>
2014-02-28 09:54:16 +0000

    Initial Commit
    Included NetBeans project

---
+++ WorldCupApp/src/nci/OOPGroup03/WCAMainController.java
@@ -0,0 +1,26 @@
+/*
+ * WorldCupApp
+ * WCAMainController.java
+ * 
+ * Created by Niall Quinn on Feb 28, 2014
+ * This Project is created for NCI Object-Oriented Programming Year 1, Group Assignment.
+ * Copyright (c) Niall Quinn, Jonathan James Turner, James Walsh, John O'Shea. All rights reserved.
+ * 
+ */
+
+package nci.OOPGroup03;
+
+/**
+ *
+ * @author Niall Quinn
+ */
+public class WCAMainController {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
